9d5e4dd3a5ffb5f3eb8e10db3437748a01470e9b
Caio <caiomgp22@gmail.com>
2021-07-29 13:48:18 -0300

    Correçao exercicio10

--- M1_Fundamentos/bloco_4/dia-1-javascript-primeiros-passos/exercicio10.js
+++ M1_Fundamentos/bloco_4/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -3,9 +3,10 @@ let venda = 200;
 let imposto = 0.2 * custo;
 let custoTotal = custo + imposto;
 let lucro = venda - custoTotal;
+let lucroTotal = 1000 * lucro;
 
 if (custo < 0 || venda < 0) {
     console.log("Valores de entrada inválidos")
 } else {
-    console.log(lucro)
+    console.log(lucroTotal)
 }
