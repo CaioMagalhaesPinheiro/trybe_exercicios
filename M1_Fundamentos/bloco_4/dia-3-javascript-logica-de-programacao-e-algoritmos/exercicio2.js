12ddb3d7bca257aab277c03686f4fee16c595c81
Caio <caiomgp22@gmail.com>
2021-07-30 18:08:28 -0300

    exercicio2

---
+++ M1_Fundamentos/bloco_4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,10 @@
+/* 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo 
+com 5 asteriscos de base. */
+
+let n = 5;
+let linhaAsteristico = "";
+
+for (let index = 1; index <= n; index += 1) {
+    linhaAsteristico += "*";
+    console.log(linhaAsteristico)
+}
