a122bf5ed4da37fee2d5ab5fbb0e74d54b4e5c58
Caio <caiomgp22@gmail.com>
2021-08-02 15:57:46 -0300

    correçao do exercicio1

--- M1_Fundamentos/bloco_4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
+++ M1_Fundamentos/bloco_4/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
@@ -2,11 +2,22 @@
 
 let n = 5;
 let linhaAsteristico = "";
+let espaçoVazio = "";
+let triangulo;
 
 if (n <= 1) {
   console.log("Número inválido.")
 } else {
-    for (let index = 1; index <= n; index += 1) {
-      linhaAsteristico += "";
-      }                   
-    }
+  for (let index = n-1; index > 0; index -= 1) {
+    espaçoVazio += "X";
+  }
+  for (let index2 = 1; index2 <= n; index2 += 1) {
+    espaçoVazio += " ";
+    console.log(espaçoVazio);
+  }
+  for (let index2 = 1; index2 <= n; index2 += 1) {
+  linhaAsteristico += "*";
+  triangulo = espaçoVazio + linhaAsteristico;
+  console.log(triangulo)
+  }  
+}
