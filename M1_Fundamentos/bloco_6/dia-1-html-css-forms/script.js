409e05a4d4711955de4a11f92c9fec8c413bf842
Caio <caiomgp22@gmail.com>
2021-08-19 11:01:43 -0300

    Adicionando options dos estados

---
+++ M1_Fundamentos/bloco_6/dia-1-html-css-forms/script.js
@@ -0,0 +1,11 @@
+const selectState = document.getElementById('state');
+let stateList = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+function putState() {
+  for (let index in stateList) {
+    const stateItem = document.createElement('option');
+    stateItem.value = stateList[index];
+    stateItem.innerText = stateList[index];
+    selectState.appendChild(stateItem);
+  }
+} putState();
