fa807561aab48f398d3af401835cf1b3ab5c953d
Caio <caiomgp22@gmail.com>
2021-08-30 19:46:39 -0300

    Parte 2 - 6 ao 8

--- M1_Fundamentos/bloco_7/dia-2-javascript-es6-objects/Parte2.js
+++ M1_Fundamentos/bloco_7/dia-2-javascript-es6-objects/Parte2.js
@@ -42,3 +42,27 @@ listValues(lesson3);
 const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 } );
 
 console.log(allLessons);
+
+function numberOfStudents() {
+  const studentsL1 = lesson1.numeroEstudantes;
+  const studentsL2 = lesson2.numeroEstudantes;
+  const studentsL3 = lesson3.numeroEstudantes;
+  const studentsNumber = studentsL1 + studentsL2 + studentsL3;
+  console.log(studentsNumber);
+}
+numberOfStudents();
+
+const getValueByNumber = (obj,number) => Object.values(obj)[number];
+console.log(getValueByNumber(lesson1, 0));
+
+function verify(object, chave, valor) {
+  const arrayChave = Object.keys(object);
+  const arrayValor = Object.values(object);
+  if (arrayChave.includes(chave) || arrayValor.includes(valor)) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+verify(lesson1, 'noite', 'turno');
+verify(lesson3, 'materia', 'Maria Clara');
