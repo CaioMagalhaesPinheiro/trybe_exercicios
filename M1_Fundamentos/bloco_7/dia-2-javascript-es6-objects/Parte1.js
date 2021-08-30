47c47a82da4d5f4f374bc8479898c1692a5550ad
Caio <caiomgp22@gmail.com>
2021-08-30 18:40:21 -0300

    Parte 1

---
+++ M1_Fundamentos/bloco_7/dia-2-javascript-es6-objects/Parte1.js
@@ -0,0 +1,62 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customerName = order.name;
+  const customerPhone = order.phoneNumber;
+  const address = 'address';
+  const street = order.address.street;
+  const number = order.address.number;
+  const apartment = order.address.apartment;
+  
+  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, ${street}, N°: ${number}, AP: ${apartment}`);
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  let customerName = order.name;
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  let payment = order.payment.total;
+  customerName = 'Luiz Silva';
+  payment = '50';
+
+  console.log(`Olá ${customerName}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${payment},00.`);
+}
+
+orderModifier(order);
