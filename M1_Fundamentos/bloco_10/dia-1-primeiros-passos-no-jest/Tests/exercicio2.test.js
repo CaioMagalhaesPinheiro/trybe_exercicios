880848e03bec3ddcca1033b5bc7a353827bcb16d
Caio <caiomgp22@gmail.com>
2021-09-22 10:47:33 -0300

    Exercicio 2 de testes jest

---
+++ M1_Fundamentos/bloco_10/dia-1-primeiros-passos-no-jest/Tests/exercicio2.test.js
@@ -0,0 +1,27 @@
+const myRemove = require('../Exercises/exercicio2.js')
+
+/* A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+Verifique se o array passado por parâmetro não sofreu alterações
+Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado */
+
+describe('A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array', () => {
+  it('Verifica se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('Verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  const array = [5, 6, 7, 8];
+  myRemove(array, 7);
+  it('Verifica se o array passado por parâmetro não sofreu alterações', () => {
+    expect(array).toEqual([5, 6, 7, 8]);
+  });
+  
+  it('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+
+});
